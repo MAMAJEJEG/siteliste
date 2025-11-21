7c41c2351fdd6b33340178574b25c011b42b79d7
jeremy <jeremy.kusper@etu.unistra.fr>
2025-11-21 20:13:10 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector(".nav")
+const menuIcon = document.querySelector(".menu-icon")
+menuIcon.addEventListener('click',()=>{
+nav.classList.toggle('mobile-menu')
+console.log("salut")
+})
