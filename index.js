40aa5a87a15f350cc2d4ee23f1916f5a3a4fae28
jeremy <jeremy.kusper@etu.unistra.fr>
2025-12-25 13:23:00 +0100

    Ruche qui superposée au paysage

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ console.log("salut")
 })
 
 const ruche = document.querySelector(".ruche-image");
-
-
 const btnRetour = document.getElementById('btn-retour');
 const vueInterieure = document.getElementById('interieur-ruche');
-
+const mainElement = document.querySelector('main');
 // Quand on clique sur la ruche
 ruche.addEventListener('click', () => {
     vueInterieure.classList.add('active'); // Le CSS gère l'apparition
+    mainElement.classList.add('ruche-ouverte');
 });
 
 // Quand on clique sur "Sortir"
 btnRetour.addEventListener('click', () => {
     vueInterieure.classList.remove('active'); // Le CSS gère la disparition
+    mainElement.classList.remove('ruche-ouverte');
 });
